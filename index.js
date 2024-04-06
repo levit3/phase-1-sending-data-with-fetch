d1b2a870995b6aa623febbedb264360150554f6f
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-06 16:09:54 +0300

    adds post request that updated response and errors at the DOM

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = { name: name, email: email };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then((res) => res.json())
+    .then((users) => appendDom(users))
+    .catch((error) => catchError(error));
+}
+
+function appendDom(users) {
+  const body = document.querySelector("body");
+  body.innerHTML = `${users.id}`;
+}
+
+function catchError(error) {
+  const body = document.querySelector("body");
+  body.innerHTML = error.message;
+}
+
+document.addEventListener("DOMContentLoaded", submitData);
